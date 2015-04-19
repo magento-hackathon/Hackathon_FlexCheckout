ded5942b1125cad50951bbfd3b52c3b0f0ecab08
Maciej Ostrowski <mostrowski@creatuity.pl>
2015-04-19 13:46:54 -0500

    Hackathon_FlexCheckout module

---
+++ app/code/community/Hackathon/FlexCheckout/controllers/FlexCheckoutController.php
@@ -0,0 +1,10 @@
+<?php
+
+class Hackathon_FlexCheckout_FlexCheckoutController extends Mage_Core_Controller_Front_Action
+{
+    
+    public function indexAction() {
+        echo ('test');
+    }
+    
+}
